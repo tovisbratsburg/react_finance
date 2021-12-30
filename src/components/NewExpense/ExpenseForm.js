5e753252c2c9ccdbaba1399190f1df210b5da37c
Tovis Bratsburg <tovisbratsburg@gmail.com>
2021-12-30 18:45:58 -0500

    add 2 way binding

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -6,7 +6,7 @@ const ExpenseForm = () => {
   const [enteredTitle, setEnteredTitle] = useState('');
   const [enteredAmount, setEnteredAmount] = useState('');
   const [enteredDate, setEnteredDate] = useState('');
-
+  
   const titleChangeHandler = (event) => {
     setEnteredTitle(event.target.value);
   };
@@ -19,12 +19,26 @@ const ExpenseForm = () => {
     setEnteredDate(event.target.value);
   };
 
+  const submitHandler = (event) => {
+    event.preventDefault();
+    
+    const expenseDate = {
+        title: enteredTitle,
+        amount: enteredAmount,
+        date: new Date(enteredDate)
+    };
+
+  };
+
   return (
-    <form>
+    <form onSubmit={submitHandler}>
       <div className='new-expense__controls'>
         <div className='new-expense__control'>
           <label>Title</label>
-          <input type='text' onChange={titleChangeHandler} />
+          <input type='text' 
+            value={enteredTitle}
+            onChange={titleChangeHandler} 
+          />
         </div>
         <div className='new-expense__control'>
           <label>Amount</label>
@@ -32,6 +46,7 @@ const ExpenseForm = () => {
             type='number'
             min='0.01'
             step='0.01'
+            value={enteredAmount}
             onChange={amountChangeHandler}
           />
         </div>
@@ -41,6 +56,7 @@ const ExpenseForm = () => {
             type='date'
             min='2019-01-01'
             max='2022-12-31'
+            value={enteredDate}
             onChange={dateChangeHandler}
           />
         </div>
